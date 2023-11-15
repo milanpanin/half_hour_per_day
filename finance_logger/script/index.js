6caa8ba5972e81de81ad2abdb38fd2edbe9910ae
milanpanin <milanpanin@gmail.com>
2023-11-15 14:40:02 +0100

    Adding table for finance app

--- finance_logger/script/index.js
+++ finance_logger/script/index.js
@@ -3,7 +3,19 @@ var finance_to_from = document.getElementById('finance_to_from');
 var finance_details = document.getElementById('finance_details');
 var finance_amount = document.getElementById('finance_amount');
 var finance_submit = document.getElementById('finance_submit');
+var finance_table = document.getElementById('finance_table_body');
+var printNewFinance = function (financeBlock) {
+    var financeRow = document.createElement('tr');
+    financeRow.innerHTML = "\n    <td>".concat(financeBlock.type, "</td>\n    <td>").concat(financeBlock.to_from, "</td>\n    <td>").concat(financeBlock.details, "</td>\n    <td>").concat(financeBlock.amount, "</td>\n  ");
+    finance_table.appendChild(financeRow);
+};
 finance_submit.addEventListener('click', function (event) {
     event.preventDefault();
-    console.log(finance_type.value, finance_to_from.value, finance_details.value, finance_amount.value);
+    var financeBlock = {
+        type: finance_type.value,
+        to_from: finance_to_from.value,
+        details: finance_details.value,
+        amount: finance_amount.valueAsNumber,
+    };
+    printNewFinance(financeBlock);
 });
--- finance_logger/script/index.ts
+++ finance_logger/script/index.ts
@@ -3,9 +3,36 @@ const finance_to_from = document.getElementById('finance_to_from') as HTMLInputE
 const finance_details = document.getElementById('finance_details') as HTMLInputElement;
 const finance_amount = document.getElementById('finance_amount') as HTMLInputElement;
 const finance_submit = document.getElementById('finance_submit') as HTMLButtonElement;
+const finance_table = document.getElementById('finance_table_body') as HTMLTableElement;
 
+type finance_type = {
+  type: string,
+  to_from: string,
+  details: string,
+  amount: number,
+}
+
+const printNewFinance = (financeBlock: finance_type) => {
+  const financeRow = document.createElement('tr');
+  financeRow.innerHTML = `
+    <td>${financeBlock.type}</td>
+    <td>${financeBlock.to_from}</td>
+    <td>${financeBlock.details}</td>
+    <td>${financeBlock.amount}</td>
+  `;
+
+  finance_table.appendChild(financeRow);
+};
 
 finance_submit.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(finance_type.value, finance_to_from.value, finance_details.value, finance_amount.value);
+
+  const financeBlock = {
+    type: finance_type.value,
+    to_from: finance_to_from.value,
+    details: finance_details.value,
+    amount: finance_amount.valueAsNumber,
+  };
+
+  printNewFinance(financeBlock);
 });

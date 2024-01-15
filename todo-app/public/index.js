a00c0de8308f414b5e6aa7a93d3beefbd6c289da
milanpanin <milanpanin@gmail.com>
2024-01-15 14:37:15 +0100

    Implement form and table

--- todo-app/public/index.js
+++ todo-app/public/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Hello World');
+document.getElementById('todo-container__submit').addEventListener('click', (event) => {
+    event.preventDefault();
+    // TODO: Validacija unetih podataka
+    const product = document.getElementById('todo-container__product');
+    const count = document.getElementById('todo-container__count');
+    console.log(product.value, count.value);
+});
--- todo-app/src/index.ts
+++ todo-app/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+document.getElementById('todo-container__submit')!.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // TODO: Validacija unetih podataka
+  const product = document.getElementById('todo-container__product') as HTMLInputElement;
+  const count = document.getElementById('todo-container__count') as HTMLInputElement;
+
+  console.log(product.value, count.value);
+});

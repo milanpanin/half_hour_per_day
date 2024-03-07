6496f819bc2b806602841d46fbc6e5aa2b95576d
milanpanin <milanpanin@gmail.com>
2024-03-07 14:28:53 +0100

    Implement FS and save in txt file

--- oop-task-1/src/index.ts
+++ oop-task-1/src/index.ts
@@ -2,6 +2,7 @@
 import { Jabuka } from './classes/Jabuka.js';
 import { Cediljka } from './classes/Cediljka.js';
 import { Posuda } from './classes/Posuda.js';
+import * as fs from 'fs';
 
 const posuda = new Posuda(200);
 
@@ -16,16 +17,16 @@ const dodajJabukeUPosudu = (brojJabuka: number) => {
 
 dodajJabukeUPosudu(1000);
 
-console.log('\n---------------- INFO ----------------');
-console.log(`Broj dodatih voćki u posudi: ${posuda.brojDodatihVocki()}.`);
-console.log(`Slobodni kapacitet posude: ${posuda.slobodnoMesta()}.`);
-console.log(`Dostupna tezina posude: ${posuda.dostupnaTezinaPosude()} grama.`);
-console.log('---------------- INFO ---------------- \n');
+// Kreiranje poruke za ispis
+let poruka = `Broj dodatih voćki u posudi: ${posuda.brojDodatihVocki()}.\n`;
+poruka += `Slobodni kapacitet posude: ${posuda.slobodnoMesta()}.\n`;
+poruka += `Dostupna tezina posude: ${posuda.dostupnaTezinaPosude()} grama.\n`;
 
+// Ceđenje
 const cediljka = new Cediljka(posuda);
-
-console.log('\n---------------- Ceđenje ----------------');
+poruka += '\nStartovanje ceđenja! \n\n';
 cediljka.cedi();
-console.log('---------------- Ceđenje ---------------- \n');
 
-console.log(`Dobijeno soka: ${cediljka.dobijenoSoka().toFixed(2)} ml (od prilike).`);
+poruka += `Dobijeno soka: ${cediljka.dobijenoSoka().toFixed(2)} ml (od prilike).\n`;
+
+fs.writeFileSync('status.txt', poruka);
